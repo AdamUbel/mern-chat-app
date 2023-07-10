54328fc9a6e758f511a7eb011bb6d1bdc1e0301a
Adam <92753806+AdamUbel@users.noreply.github.com>
2023-07-09 21:49:34 -0700

    Added notes/comments

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,13 +1,18 @@
 const { User } = require("../models/user.model");
 const bcrypt = require("bcrypt");
 
+// Controller function to register a new user
 module.exports.registerUser = async (req, res, next) => {
   try {
     const { username, email, password, picture } = req.body;
+
+    // Checking if the username already exists
     const checkUsername = await User.findOne({ username });
     if (checkUsername) {
       return res.status(400).json({ errors: { usernameExists: { message: "Username taken." } } });
     }
+
+    // Checking if the email already exists
     const checkEmail = await User.findOne({ email });
     if (checkEmail) {
       return res.status(400).json({ errors: { emailExists: { message: "Email taken." } } });
@@ -18,6 +23,7 @@ module.exports.registerUser = async (req, res, next) => {
       hashedPassword = await bcrypt.hash(password, 10);
     }
 
+    // Creating a new user with the hashed password
     const user = await new User({ username, email, password: hashedPassword, picture });
 
     user
@@ -38,13 +44,18 @@ module.exports.registerUser = async (req, res, next) => {
   }
 };
 
+// Controller function to login a user
 module.exports.loginUser = async (req, res, next) => {
   try {
     const { username, password } = req.body;
+
+    // Finding the user based on the provided username
     const user = await User.findOne({ username });
     if (!user) {
       return res.status(400).json({ errors: { invalidLogin: { message: "Invalid Username Or Password." } } });
     }
+
+    // Validating the password
     const validPassword = await bcrypt.compare(password, user.password);
     if (!validPassword) {
       return res.status(400).json({ errors: { invalidLogin: { message: "Invalid Username Or Password." } } });
@@ -57,6 +68,7 @@ module.exports.loginUser = async (req, res, next) => {
   }
 };
 
+// Controller function to get all users
 module.exports.allUsers = (req, res) => {
   User.find({ username: { $exists: true } })
     .sort({ username: 1 })
